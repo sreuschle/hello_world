b07ddef602e42e875d5fc9f7118477f84e218a62
Sarina Reuschle <sarina213@Sarinas-MacBook-Air.local>
2017-10-09 16:20:24 -0700

    Outputs sarina to the screen

---
+++ hello_world.rb
@@ -0,0 +1,20 @@
+
+
+class HelloWorld
+	def initialize(name)
+		@name = name
+	end
+
+	def say_something
+		puts @name
+	end
+end
+
+hello_world = HelloWorld.new("sarina")
+hello_world.say_something
+
+
+
+
+
+
