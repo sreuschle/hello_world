3df69c4ef7f7b2c29125c2c19277e286cfee83db
Sarina Reuschle <sarina213@Sarinas-Air.local.tld>
2017-10-09 17:28:55 -0700

    outputs learning is fun to screen

--- hello_world.rb
+++ hello_world.rb
@@ -1,16 +1,16 @@
 
 
 class HelloWorld
-	def initialize(name)
-		@name = name
+	def initialize(string_to_output)
+		@string_to_output = string_to_output
 	end
 
 	def say_something
-		puts @name
+		puts @string_to_output
 	end
 end
 
-hello_world = HelloWorld.new("sarina")
+hello_world = HelloWorld.new("learning is fun")
 hello_world.say_something
 
 
